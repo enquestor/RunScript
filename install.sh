be9d7a30882df4c6bd8b62c12d2637dd02b7aad7
Allen Hu <allenhu1029@gmail.com>
2020-04-22 10:06:05 +0800

    Changed default run, add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+readonly username=$(who am i | awk '{ print $1 }')
+readonly runpath="/Users/$username/.runsciprt"
+
+# remove if folder exists
+installed=$(ls -al /Users/"$username"/ | grep .runscript | wc -l | awk '{ print $1 }')
+if [ "$installed" = 1 ] ; then
+	rm -rf "$runpath"
+fi
+
+# get source files
+git clone https://github.com/Allen-Hu/RunScript.git "$runpath"
+
+# add folder to path
+has_path=$(cat /etc/paths | grep "runpath" | wc -l | awk '{ print $1 }')
+if [ "$has_path" = 0 ] ; then
+	sh -c "echo $runpath >> /etc/paths"
+fi
+
+# give executing permissions
+chmod a+x "$runpath"/run
